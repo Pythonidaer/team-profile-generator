67e422d25a5e83a8a75f89aa82f9a1efb3382d07
Pythonidaer <pythonidaer@gmail.com>
2021-02-21 23:42:53 -0500

    Need to modulate html template chunks into their own html files. Then I will save around 80 lines of JavaScript and have much easier to read code. Once I finish creating a GitHub page for this working product, I would like to update my README for completion, followed by learning about Jest documentation so I can begin building my unit tests.

--- README.md
+++ README.md
@@ -9,10 +9,15 @@
 [CSS Tricks: Box Shadow](https://css-tricks.com/almanac/properties/b/box-shadow/)
 
 [Bootstrap](https://getbootstrap.com/)
-[]()
-[]()
-[]()
-[]()
+
+[fs.readFile](https://nodejs.org/dist/latest-v10.x/docs/api/fs.html#fs_fs_readfile_path_options_callback)
+
+[Object.getOwnPropertyNames()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/getOwnPropertyNames)
+
+[fs.appendFile()](https://nodejs.org/dist/latest-v10.x/docs/api/fs.html#fs_fs_appendfile_path_data_options_callback)
+
+[fs.writeFile()](https://nodejs.org/dist/latest-v10.x/docs/api/fs.html#fs_fs_write_fd_string_position_encoding_callback)
+
 []()
 []()
 []()
--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const fs = require('fs');
 const Manager = require('./lib/Manager.js');
 const Engineer = require('./lib/Engineer.js');
 const Intern = require('./lib/Intern.js');
+// every card content goes into empty array
 const teamMembers = [];
 
+// user menu to askEngineer or askIntern questions, otherwise, create html and append cards depending on member type, then append closing html and console.log "HTML created!" or something
 function memberMenu() {
     inquirer
         .prompt([
@@ -28,7 +30,116 @@ function memberMenu() {
                     askIntern();
                     break;
                 case "I don't want to add any more team members":
-                    console.log(teamMembers);
+
+                    const indexHTML = './dist/index.html';
+
+                    const startingHTML = `
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <script src="https://kit.fontawesome.com/8af330bb1e.js" crossorigin="anonymous"></script>
+        <title>Team Profile Generator</title>
+    </head>
+    <body>
+        <header class="bg-danger">
+            <h1 class="text-light text-center py-5">My Team</h1>
+        </header>
+
+        <!-- Cards to go into card deck -->
+        <div class="container w-sm-75 mt-5">
+            <div class="d-flex flex-wrap justify-content-center">`;
+
+                    fs.writeFile(indexHTML, startingHTML, (err) => {
+                        if (err) throw err;
+                        console.log('The file has been saved!');
+                    });
+
+
+                    // the second thing in this case statement should be the forEach that triggers fs.appendFile
+                    // for every team member in the array, build a card depending on their role.
+                    // the bottom case console.logs should be changed into buildManager(), buildEngineer() and buildIntern()
+                    teamMembers.forEach(member => {
+                        switch (member.getRole()) {
+                            case "Manager":
+                                const managerCard = `
+                <div class="card mb-4 mr-sm-4 bg-light" style="width: 17rem; box-shadow: 6px 5px 5px 1px #888!important">
+                    <div class="card-header bg-primary" style="margin-right: -1px;">
+                        <h2 class="text-light">${member.name}</h2>
+                        <h3 class="text-light"><i class="fas fa-mug-hot pr-2"></i>${member.getRole()}</h3>
+                    </div>
+                    <div class="bg-light py-4 px-3">            
+                        <ul class="list-group list-group-flush border rounded" style="border: 1px solid rgba(0,0,0,.125)">
+                            <li class="list-group-item">ID: ${member.id}</li>
+                            <li class="list-group-item">Email: <a href="mailto:${member.email}?" target="_blank" rel="noopener">${member.email}</a></li>
+                            <li class="list-group-item">Officer number: ${member.officeNumber}</li>
+                        </ul>
+                    </div>
+                </div>`;
+                
+                    fs.appendFile(indexHTML, managerCard, (err) => {
+                        if (err) throw err;
+                        console.log('The "data to append" was appended to file!');
+                    });
+                                break;
+                            case "Engineer":
+                                const engineerCard = `
+                <div class="card mb-4 mr-sm-4 bg-light" style="width: 17rem; box-shadow: 6px 5px 5px 1px #888!important">
+                    <div class="card-header bg-primary" style="margin-right: -1px;">
+                        <h2 class="text-light">${member.name}</h2>
+                        <h3 class="text-light"><i class="fas fa-glasses pr-2"></i>${member.getRole()}</h3>
+                    </div>
+                    <div class="bg-light py-4 px-3">            
+                        <ul class="list-group list-group-flush border rounded" style="border: 1px solid rgba(0,0,0,.125)">
+                            <li class="list-group-item">ID: ${member.id}</li>
+                            <li class="list-group-item">Email: <a href="mailto:${member.email}" target="_blank" rel="noopener">${member.email}</a></li>
+                            <li class="list-group-item">GitHub: <a href="https://github.com/${member.github}" target="_blank" rel="noopener">${member.github}</a></li>
+                        </ul>
+                    </div>
+                </div>`;
+                
+                    fs.appendFile(indexHTML, engineerCard, (err) => {
+                        if (err) throw err;
+                        console.log('The "data to append" was appended to file!');
+                    }); 
+                                break;
+                            case "Intern":
+                                const internCard = `
+                <div class="card mb-4 mr-sm-4 bg-light" style="width: 17rem; box-shadow: 6px 5px 5px 1px #888!important">
+                    <div class="card-header bg-primary" style="margin-right: -1px;">
+                        <h2 class="text-light">${member.name}</h2>
+                        <h3 class="text-light"><i class="fas fa-user-graduate pr-2"></i>${member.getRole()}</h3>
+                    </div>
+                    <div class="bg-light py-4 px-3">            
+                        <ul class="list-group list-group-flush border rounded" style="border: 1px solid rgba(0,0,0,.125)">
+                            <li class="list-group-item">ID: ${member.id}</li>
+                            <li class="list-group-item">Email: <a href="mailto:${member.email}" target="_blank" rel="noopener">${member.email}</a></li>
+                            <li class="list-group-item">School: ${member.school}</li>
+                        </ul>
+                    </div>
+                </div>`;
+                
+                    fs.appendFile(indexHTML, internCard, (err) => {
+                        if (err) throw err;
+                        console.log('The "data to append" was appended to file!');
+                        }); 
+                                break;
+                        }
+                    })
+
+                // the third thing in this case statement should be the ending of the html, so fs.appendFile
+                const closingHTML = `
+            </div>
+        </div>              
+    </body>
+</html>`;
+            fs.appendFile(indexHTML, closingHTML, (err) => {
+                if (err) throw err;
+                console.log('The "data to append" was appended to file!');
+              });    
                     break;
             }  
         });
@@ -73,7 +184,7 @@ inquirer
 
         memberMenu()
  
-        const index = './output/index.html';
+        // const index = './output/index.html';
 
         // fs.writeFile(index, answers, (err) =>
         //     err ? console.log(err) : console.log('Success!')
